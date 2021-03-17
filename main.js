60cf22e1d802dc92c7752229f88f7fc6bab3d38e
Juhyun <s2019w09@e-mirim.hs.kr>
2021-03-18 08:40:14 +0900

    web2-11 동적페이지 기초

--- main.js
+++ main.js
@@ -5,15 +5,35 @@ const url = require('url');
 const app = http.createServer(function(req, res){
    let _url = req.url;
    const queryData = url.parse(_url, true).query;
+   let title = queryData.id;
    console.log(queryData.id);
    if(_url == '/') {
-      _url = '/index.html';
+      title = 'Welcome';
    }
    if(_url == '/favicon.ico') {
       return res.writeHead(404);
    }
    res.writeHead(200);
-   res.end(queryData.id);
+   const template = `
+    <!doctype html>
+    <html>
+    <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+            <li><a href="/?id=HTML">HTML</li>
+            <li><a href="/?id=CSS">CSS</li>
+            <li><a href="/?id=JavaScript">JavaScript</li>
+        </ul>
+        <h2>${title}</h2>
+        
+    </body>
+    </html>
+    `;
+   res.end(template);
 });
 
 app.listen(3000);
