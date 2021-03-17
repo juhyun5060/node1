c9d60b189947af7b8723cbb6241ba4e15deae6ed
Juhyun <s2019w09@e-mirim.hs.kr>
2021-03-17 09:12:34 +0900

    web2-5

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
 const app = http.createServer(function(req, res){
+   var _url = req.url;
+   var queryData = url.parse(_url, true).query;
+   console.log(queryData.id);
+   if(_url == '/') {
+      _url = '/index.html';
+   }
+   if(_url == '/favicon.ico') {
+      return res.writeHead(404);
+   }
    res.writeHead(200);
-   console.log(__dirname+req.url);
-   res.end("hello egoing");
+   res.end(fs.readFileSync(__dirname + url));
 });
 
 app.listen(3000);
