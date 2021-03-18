c1e86f4e7c57532302bdbb1c8113231326786a0c
Juhyun <s2019w09@e-mirim.hs.kr>
2021-03-18 09:56:18 +0900

    web2-19 동적페이지

--- main.js
+++ main.js
@@ -1,39 +1,67 @@
 const http = require('http');
-// const fs = require('fs');
+const fs = require('fs');
 const url = require('url');
 
-const app = http.createServer(function(req, res){
-   let _url = req.url;
-   const queryData = url.parse(_url, true).query;
-   let title = queryData.id;
-   console.log(queryData.id);
-   if(_url === '/') {
-      title = 'Welcome';
-   }
-   if(_url === '/favicon.ico') {
-      return res.writeHead(404);
-   }
-   res.writeHead(200);
-   const template = `
-    <!doctype html>
-    <html>
-    <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-            <li><a href="/?id=HTML">HTML</li>
-            <li><a href="/?id=CSS">CSS</li>
-            <li><a href="/?id=JavaScript">JavaScript</li>
-        </ul>
-        <h2>${title}</h2>
-        
-    </body>
-    </html>
-    `;
-   res.end(template);
-});
+var app = http.createServer(function (request, response) {
+    const _url = request.url;
+    const queryData = url.parse(_url, true).query;
+    const pathname = url.parse(_url, true).pathname;
+    if (pathname === '/') {
+        if (queryData.id === undefined) {
+            let title = 'Welcome';
+            let description = 'Hello, Node.js';
+            let template = `
+          <!doctype html>
+          <html lang="ko">
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+            response.writeHead(200);
+            response.end(template);
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                var title = queryData.id;
+                var template = `
+          <!doctype html>
+          <html lang="ko">
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
+    }
 
+
+});
 app.listen(3000);
