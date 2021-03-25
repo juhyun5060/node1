9fd6c84ef4cd84eb463db728d384bb3760c97bcf
Juhyun <s2019w09@e-mirim.hs.kr>
2021-03-25 09:45:28 +0900

    web2-24 글 목록

--- main.js
+++ main.js
@@ -10,53 +10,63 @@ const app = http.createServer(function (request, response) {
         if (queryData.id === undefined) {
             let title = 'Welcome';
             let description = 'Hello, Node.js';
-            let template = `
-          <!doctype html>
-          <html lang="ko">
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            
-            <!--<ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>-->
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
-            response.writeHead(200);
-            response.end(template);
-        } else {
-            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-                var title = queryData.id;
-                var template = `
-                <!doctype html>
-                <html lang="ko">
-                <head>
+
+            fs.readdir('./data', function(err, data){
+                let list='<ul>';
+
+                for(let i=0; i<data.length; i++) {
+                    list = list + `<li><a href="/?id=${data[i]}">${data[i]}</a></li>`;
+                }
+                list += '</ul>';
+                const template = `
+                  <!doctype html>
+                  <html lang="ko">
+                  <head>
                     <title>WEB1 - ${title}</title>
                     <meta charset="utf-8">
-                </head>
-                <body>
+                  </head>
+                  <body>
                     <h1><a href="/">WEB</a></h1>
-                    <ul>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
-                    </ul>
+                    ${list}
                     <h2>${title}</h2>
                     <p>${description}</p>
-                </body>
-                </html>
+                  </body>
+                  </html>
                 `;
                 response.writeHead(200);
                 response.end(template);
+            })
+
+        } else {
+            fs.readdir('./data', function(err, data) {
+                let list = '<ul>';
+
+                for (let i = 0; i < data.length; i++) {
+                    list = list + `<li><a href="/?id=${data[i]}">${data[i]}</a></li>`;
+                }
+                list += '</ul>';
+                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                    var title = queryData.id;
+                    var template = `
+                        <!doctype html>
+                        <html lang="ko">
+                        <head>
+                            <title>WEB1 - ${title}</title>
+                            <meta charset="utf-8">
+                        </head>
+                        <body>
+                            <h1><a href="/">WEB</a></h1>
+                            ${list}
+                            <h2>${title}</h2>
+                            <p>${description}</p>
+                        </body>
+                        </html>
+                    `;
+                    response.writeHead(200);
+                    response.end(template);
+                });
             });
+
         }
     } else {
         response.writeHead(404);
