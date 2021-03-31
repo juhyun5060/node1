2ef892c8d41bf837b8f78a2fd708ff8e649dc1dc
Juhyun <s2019w09@e-mirim.hs.kr>
2021-03-31 09:38:01 +0900

    web2-31 글생성-UI

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ function templateHTML(title, list, body) {
       <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
+         <a href="/create">create</a>
         ${body}
       </body>
       </html>
@@ -99,6 +100,24 @@ const app = http.createServer(function (request, response) {
             });
 
         }
+    } else if(pathname === '/create'){
+        fs.readdir('./data', function(error, filelist){
+            const title = 'WEB - create';
+            const list = templateList(filelist);
+            const template = templateHTML(title, list, `
+          <form action="/process_create" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `);
+            response.writeHead(200);
+            response.end(template);
+        });
     } else {
         response.writeHead(404);
         response.end('Not found');
