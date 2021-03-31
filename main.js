2e81f44cbf957f8f0973cbced1c111cf800cd678
Juhyun <s2019w09@e-mirim.hs.kr>
2021-03-31 10:22:29 +0900

    web2-34 글수정-수정링크 생성

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function templateList(filelist) {
     return list;
 }
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
     return `
       <!doctype html>
       <html lang="ko">
@@ -24,7 +24,7 @@ function templateHTML(title, list, body) {
       <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
-         <a href="/create">create</a>
+        ${control}
         ${body}
       </body>
       </html>
@@ -42,7 +42,7 @@ const app = http.createServer(function (request, response) {
 
             fs.readdir('./data', function(err, data){
                 const list = templateList(data);
-                const template = templateHTML(title, list, `<h2>${title}</h2><br>${description}`);
+                const template = templateHTML(title, list, `<h2>${title}</h2><br>${description}`, `<a href="/create">create</a>`);
 
                 /*for(let i=0; i<data.length; i++) {
                     list = list + `<li><a href="/?id=${data[i]}">${data[i]}</a></li>`;
@@ -80,21 +80,7 @@ const app = http.createServer(function (request, response) {
 
                 fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
                     var title = queryData.id;
-                    var template = `
-                        <!doctype html>
-                        <html lang="ko">
-                        <head>
-                            <title>WEB1 - ${title}</title>
-                            <meta charset="utf-8">
-                        </head>
-                        <body>
-                            <h1><a href="/">WEB</a></h1>
-                            ${list}
-                            <h2>${title}</h2>
-                            <p>${description}</p>
-                        </body>
-                        </html>
-                    `;
+                    var template = templateHTML(title, list, `<h2>${title}</h2><br>${description}`, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
                     response.writeHead(200);
                     response.end(template);
                 });
@@ -115,7 +101,7 @@ const app = http.createServer(function (request, response) {
               <input type="submit">
             </p>
           </form>
-        `);
+        `, '');
             response.writeHead(200);
             response.end(template);
         });
@@ -128,9 +114,11 @@ const app = http.createServer(function (request, response) {
             const post = qs.parse(body);
             const title = post.title;
             const description = post.description;
+            fs.writeFile(`data/${title}`, description, 'utf-8', function(err){
+                response.writeHead(302, {Location: `/?id=${title}`});
+                response.end();
+            })
         });
-        response.writeHead(200);
-        response.end('success');
     } else {
         response.writeHead(404);
         response.end('Not found');
